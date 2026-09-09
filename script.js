d73f6f8efa1df45744b83dabd69e7edbe3c79f59
ShazAir123 <sharonairewele@gmail.com>
2026-09-09 19:44:36 +0100

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-//Demonstrate how to log information to the browser's console using JavaScript. You will set up your development environment, create and link JavaScript and HTML files, write JavaScript code to log a message, and test the output in a web browser.
-
-console.log("Message to log on the console");
